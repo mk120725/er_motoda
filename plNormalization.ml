d4c66ddbeac607ca2a06b9f0afd1d628a07d42a5
motoda <vm.94n.8914@s.thers.ac.jp>
2023-11-21 11:52:10 +0900

    a

--- plNormalization.ml
+++ plNormalization.ml
@@ -3,20 +3,21 @@ open Ratio;;
 open NewSyntax;;
 
 (* n_E(Sigma) *)
-let rec norm_E (spat : newSyntax.SHspat.t) : string list =
+
+let rec norm_E (spat : SHspat.t) : string list =
   match spat with
   | Emp -> []
   | SAtom a -> []
   | SCon (s1,s2) ->
      Tools.unionLst
        (Tools.unionLst
-          (Tools.interLst (newSyntax.SHspat.lab s1) (newSyntax.SHspat.lab s2))
+          (Tools.interLst (SHspat.lab s1) (SHspat.lab s2))
           (norm_E s1))
        (norm_E s2)
   | WCon (s1,s2) ->
      Tools.unionLst (norm_E s1) (norm_E s2)
 
-let rec check_pto spat : bool =
+let rec check_pto (spat : SHspat.t) : bool =
   match spat with
   | Emp -> false
   | SAtom at ->
@@ -26,7 +27,7 @@ let rec check_pto spat : bool =
   | SCon(s1,s2) -> (check_pto s1) || (check_pto s2)
   | _ -> false
 
-let rec ls_to_pure spat =
+let rec ls_to_pure (spat : SHspat.t) : SHpure.t =
   match spat with
   | Emp -> []
   | SAtom(Pr(p,t1::t2::[])) ->
@@ -36,7 +37,7 @@ let rec ls_to_pure spat =
   | _ -> []
 
 (* n_p(Pi,L) *)
-let norm_p (pure : newSyntax.SHpure.t) (labs : string list) : newSyntax.SHpure.t =
+let rec norm_p (pure : SHpure.t) (labs : string list) : SHpure.t =
   match pure with
   | [] -> []
   | pa::rest ->
@@ -61,13 +62,13 @@ let norm_p (pure : newSyntax.SHpure.t) (labs : string list) : newSyntax.SHpure.t
       spat2 = rest of s1n
  *)
 
-let rec extract_labels (spat : newSyntax.SHspat.t) (labs : string list) =
+let rec extract_labels (spat : SHspat.t) (labs : string list) : SHspatExp.t list * SHspat.t=
   match spat with
   | Emp -> ([],Emp)
   | SAtom s -> (
     match s with
-    | Lab a p -> if List.mem a labs then ([s],Emp) else ([],spat)
-    | _ -> (Emp,spat)
+    | Lab (a,p) -> if List.mem a labs then ([s],Emp) else ([],spat)
+    | _ -> ([],spat)
   )
   | SCon(s1,s2) ->
      let (s1l,s1r) = extract_labels s1 labs in
@@ -75,27 +76,27 @@ let rec extract_labels (spat : newSyntax.SHspat.t) (labs : string list) =
      (s1l @ s2l, SCon(s1r,s2r))
   | WCon(s1,s2) -> ([],Emp)
 
-let rec check_lab a s =
+let rec check_lab a (s : SHspatExp.t) =
   match s with
-  | Lab b _ -> a = b
+  | Lab (b,_) -> a = b
   | _ -> false
 
 (* findOption : (SHspatExp -> bool) -> SHspatExp list -> SHspatExp option
    check_lab : string -> SHspatExp -> bool
    check_lab a : SHspatExp -> bool *)
 
-let rec add_permission (l1 : SHspatExp list) (l2 : SHspatExp list) =
+let rec add_permission (l1 : SHspatExp.t list) (l2 : SHspatExp.t list) : SHspat.t =
   match l1 with
   | [] -> Emp
-  | (Lab a p1) :: rest ->
-     match Tools.findOption (check_lab a) l2 with
-     | Some (Lab a p2) =
-         Scon(Lab a (Ratio.+/ p1 p2), add_permission rest l2)
-     | _ -> Emp
+  | (Lab (a,p1)) :: rest ->
+     (match Tools.findOption (check_lab a) l2 with
+     | Some (Lab (a,p2)) ->
+         SCon(SAtom(Lab (a, (p1 +/ p2))), add_permission rest l2)
+     | _ -> Emp)
   | _ -> Emp
 
 (* n_s(Sigma) *)  
-let rec norm_s (spat : newSyntax.SHspat.t) : newSyntax.SHspat.t =
+let rec norm_s (spat : SHspat.t) : SHspat.t =
   match spat with
   | Emp -> Emp
   | SAtom s -> SAtom s
@@ -103,15 +104,15 @@ let rec norm_s (spat : newSyntax.SHspat.t) : newSyntax.SHspat.t =
      let s1n = norm_s s1 in
      let s2n = norm_s s2 in
      let commonlabels =
-       Tools.interLst (newSyntax.SHspat.lab s1n) (newSyntax.SHspat.lab s2n) in
-     let (s1l,s1r) = extract_lables s1n commonlabels in
-     let (s2l,s2r) = extract_lables s2n commonlabels in
+       Tools.interLst (SHspat.lab s1n) (SHspat.lab s2n) in
+     let (s1l,s1r) = extract_labels s1n commonlabels in
+     let (s2l,s2r) = extract_labels s2n commonlabels in
      let spat1 = add_permission s1l s2l in
      SCon(spat1, SCon(s1r, s2r))
-  | Scon(s1, s2) -> Wcon(s1, s2)
+  | SCon(s1, s2) -> WCon(s1, s2)
      
 
-let normalization (phi : newSyntax.SH.t) : newSyntax.SH.t =
+let normalization (phi : SH.t) : SH.t =
   let (pure, spat) = phi in
   let nf_pure = norm_p pure (norm_E spat) in
   let nf_spat = norm_s spat in
